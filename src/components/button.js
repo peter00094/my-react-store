8f7c58172093167247fa8ed2fa1626ca7530deef
DESKTOP-2A48IJL\USER <peter00094@gmail.com>
2020-08-27 16:24:37 +0800

    first commit

---
+++ src/components/button.js
@@ -0,0 +1,25 @@
+import styled from 'styled-components'
+
+
+export const ButtonContainer = styled.button`
+    text-transform:capitalized;
+    font-sized:1.4rem;
+    border-radius:0.5rem;
+    background:transparent;
+    border:0.05rem solid var(--lightBlue);
+    border-color:${props=> 
+    props.cart ? "var(--mainYellow)":"var(--lightBlue)"};
+    color:${props=> props.cart ? "var(--mainYellow)":"var(--lightBlue)"};
+    padding: 0.2rem 0.5rem;
+    cursor:pointer;
+    margin:0.2rem 0.5rem 0.2rem 0;
+    transition:all 0.5s ease-in-out;
+    &:hover{
+
+        background:${props=> props.cart ? "var(--mainYellow)":"var(--lightBlue)"};
+        color:var(--mainBlue);
+    }
+    &:focus{
+        outline:none;
+    }
+`
--- src/index.js
+++ src/index.js
@@ -3,11 +3,14 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './App';
 import * as serviceWorker from './serviceWorker';
-
+import {BrowserRouter as Router} from 'react-router-dom';
+import {ProductProvider} from './components/context';
 ReactDOM.render(
-  <React.StrictMode>
+  <ProductProvider>
+  <Router>
     <App />
-  </React.StrictMode>,
+  </Router>
+  </ProductProvider>,
   document.getElementById('root')
 );
 
